8bea846d3efa16155d31115eb1267bf34bd479d9
0xbe1 <0xbetrue@gmail.com>
2022-09-10 23:45:37 +0800

    bump sentio-sdk (#5)

--- src/processor.test.ts
+++ src/processor.test.ts
@@ -1,11 +1,11 @@
-import { TestProcessorServer } from "@sentio/sdk/test";
+import { TestProcessorServer } from "@sentio/sdk/lib/test";
 
 describe("Test Processor", () => {
-  const service = new TestProcessorServer();
+  const service = new TestProcessorServer(() => require("./processor"), {
+    1: "https://eth-mainnet.alchemyapi.io/v2/Gk024pFA-64RaEPIawL40n__1esXJFb2",
+  });
 
   beforeAll(async () => {
-    service.setup();
-    require("./processor");
     await service.start();
   });
 
