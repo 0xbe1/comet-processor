686110cc3e28c8da98adca41c95051ad11c5f425
0xbe1 <0xbetrue@gmail.com>
2022-09-10 11:49:18 +0800

    format

--- src/processor.ts
+++ src/processor.ts
@@ -1,7 +1,8 @@
-import { CometProcessor } from './types/Comet'
+import { CometProcessor } from "./types/Comet";
 
-CometProcessor.bind({ address: '0xc3d688b66703497daa19211eedff47f25384cdc3', startBlock: 15331586 }).onWithdraw(
-  async (event, ctx) => {
-    ctx.meter.Counter('Withdraw').add(1)
-  }
-)
+CometProcessor.bind({
+  address: "0xc3d688b66703497daa19211eedff47f25384cdc3",
+  startBlock: 15331586,
+}).onWithdraw(async (event, ctx) => {
+  ctx.meter.Counter("Withdraw").add(1);
+});
